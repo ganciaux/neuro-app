32e6126ce7d53262e9e40737feab37ae13f9aa84
ganciaux <ghislain.anciaux@live.fr>
2025-03-06 23:04:59 +0100

    Add db connexion

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,22 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { PrismaClient } from '@prisma/client';
 
 dotenv.config();
 
 const PORT = process.env.PORT || '3000';
 const app = express();
+const prisma = new PrismaClient();
 
-function setupExpress() {
-    app.get('/', (req, res) => {
-        res.status(200).send('<h1>neuro backend is running</h1>');
-    });
-}
+app.get('/', async (req, res) => {
+    res.status(200).send('<h1>neuro backend is running</h1>');
+});
 
-function startServer() {
-  app.listen(PORT, () => {
+prisma.$connect()
+    .then(() => console.log('✅ Connected to PostgreSQL'))
+    .catch((err) => console.error('❌ Database connection error:', err));
+
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-  });
-}
-
-setupExpress();
-startServer();
+});
